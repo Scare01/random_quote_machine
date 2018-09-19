142ac065865b2dc96ed55f812ed5202bdc0d7a4b
Aleksei Slesarev <lexsl84@gmail.com>
2018-09-19 18:53:30 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom';
 
 import data from './quotes.json';
 
-class App extends Component {
-    render() {
-        return (<div>{
-                data.map(quote => <div>
-                    <h1>{quote.quote}</h1>
-                    <h2>{quote.author}</h2>
-                </div>)
-            }</div>);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quotes_length: 0
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
-}
+    handleClick() {
+        // change code below this line
+        this.setState({name: data.length})
+        // change code above this line
+    }
+    render() {
+        return (<div>
+            <button onClick={this.handleClick}>Click Me</button>
+            <h1>{this.state.quotes_length}</h1>
+            <h1>{data.length}</h1>
+        </div>);
+    }
+};
 
 ReactDOM.render(<App/>, document.getElementById('root'));
