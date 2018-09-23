1b784a7a35a6daec8e863db9a983797b0b297c03
Aleksei Slesarev <lexsl84@gmail.com>
2018-09-23 12:02:42 +0700

    add main box, css for main box

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 import data from './quotes.json';
 
 const quotes = [...data];
 let max = quotes.length;
 let min = 0;
-
+const firstRandomNum = Math.floor(Math.random() * (max - min + 1) + min);
 class App extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            quote: ''
+            quote: quotes[firstRandomNum]['quote'],
+            author: quotes[firstRandomNum]['author']
         };
-        this.handleClick = this.handleClick.bind(this);
+        this.newQuote = this.newQuote.bind(this);
     }
-    handleClick() {
+    newQuote() {
         // change code below this line
         const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-        this.setState({quote: quotes[randomNum]['quote']
+        this.setState({quote: quotes[randomNum]['quote'], author: quotes[randomNum]['author']
         })
         // change code above this line
     }
     render() {
-        return (<div>
-            <button onClick={this.handleClick}>Click Me</button>
-            <h1>{this.state.quote}</h1>
+        return (<wrapper id="quote-box">
+            <div id="text">{this.state.quote}</div>
+            <div id="author"></div>
+            <button id="tweet-quote"></button>
+            <button onClick={this.newQuote} id="new-quote">New quote</button>
 
-        </div>);
+        </wrapper>);
     }
 };
 
