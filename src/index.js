e76ca63e301463f2d065c669477a943b6fcb9c40
Aleksei Slesarev <lexsl84@gmail.com>
2018-09-23 11:24:33 +0700

    add random quote

--- src/index.js
+++ src/index.js
@@ -3,24 +3,31 @@ import ReactDOM from 'react-dom';
 
 import data from './quotes.json';
 
+const quotes = [...data];
+let max = quotes.length;
+let min = 0;
+
 class App extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            quotes_length: 0
+            quote: ''
         };
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
         // change code below this line
-        this.setState({name: data.length})
+        const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+        this.setState({quote: quotes[randomNum]['quote']
+        })
         // change code above this line
     }
     render() {
         return (<div>
             <button onClick={this.handleClick}>Click Me</button>
-            <h1>{this.state.quotes_length}</h1>
-            <h1>{data.length}</h1>
+            <h1>{this.state.quote}</h1>
+
         </div>);
     }
 };
